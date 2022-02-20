18fe5a360a2eb988c00ac68d38dc2f745ed57d8a
ACristoff <acristoffanini@gmail.com>
2022-02-20 15:06:25 -0800

    setup of basic react component tree

---
+++ gamerdle-client/src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import { Container } from '@material-ui/core'
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <Container className='footer'>
+      Footer
+    </Container>
+  )
+}
+
+export default Footer
