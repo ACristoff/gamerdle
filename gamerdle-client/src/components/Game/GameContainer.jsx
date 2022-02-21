967a18630d38cdea74fe15c36893b4927aebee36
ACristoff <acristoffanini@gmail.com>
2022-02-21 14:37:09 -0800

    guess input front end implementation

--- gamerdle-client/src/components/Game/GameContainer.jsx
+++ gamerdle-client/src/components/Game/GameContainer.jsx
@@ -1,16 +1,59 @@
-import React from 'react'
-import CurrentGuesses from './CurrentGuesses/CurrentGuesses'
-import GuessInput from './GuessInput/GuessInput'
+import React, { useState } from 'react'
+
+import { TextField, Button } from '@material-ui/core'
+
 
 const GameContainer = () => {
+
+  const [guess, setGuess] = useState('');
+  const [guesses, setGuesses] = useState([]);
+
+  const handleGuessSubmit = (e) => {
+    e.preventDefault();
+    setGuesses([...guesses, guess])
+    setGuess('');
+  }
+
+  
+
   return (
     <div>
-      GameContainer
-      <CurrentGuesses />
-      <GuessInput />
+      <div className='guessData'>
+        <div className={guesses[0] ? 'GuessAnalysis' : 'BlankGuess'}>{guesses[0]}</div>
+        <div className={guesses[1] ? 'GuessAnalysis' : 'BlankGuess'}>{guesses[1]}</div>
+        <div className={guesses[2] ? 'GuessAnalysis' : 'BlankGuess'}>{guesses[2]}</div>
+        <div className={guesses[3] ? 'GuessAnalysis' : 'BlankGuess'}>{guesses[3]}</div>
+        <div className={guesses[4] ? 'GuessAnalysis' : 'BlankGuess'}>{guesses[4]}</div>
+        <div className={guesses[5] ? 'GuessAnalysis' : 'BlankGuess'}>{guesses[5]}</div>
+      </div>
+    
+      <div className='guessSubmission' style={{marginTop: '2em'}}>
+       GuessInput: {guess}
+       <form autoComplete='off' noValidate className='guessForm' onSubmit={handleGuessSubmit}>
+         <TextField placeholder='Guess a random game!' className='guessInput' onChange={(e) => setGuess(e.target.value)} value={guess}/>
+         <Button variant='contained' style={{marginTop: '20px'}} type="submit">
+           Submit Guess
+         </Button>
+       </form>
+     </div>
     </div>
-
   )
-}
+};
 
-export default GameContainer
+export default GameContainer;
+
+// sample guess data saved to localStorage
+// {"2022-02-17":[
+//   {"name":"NEW ZEALAND","distance":0,"direction":"N"}
+// ],
+// "2022-02-19":[
+//   {"name":"TURKEY","distance":5850222,"direction":"S"},
+//   {"name":"SOUTH AFRICA","distance":2004357,"direction":"NNE"},
+//   {"name":"MOZAMBIQUE","distance":1026924,"direction":"NW"},
+//   {"name":"ZAMBIA","distance":0,"direction":"N"}
+// ],
+// "2022-02-20":[
+//   {"name":"MALAYSIA","distance":7734305,"direction":"WNW"},
+//   {"name":"TURKMENISTAN","distance":1415632,"direction":"WNW"},
+//   {"name":"GEORGIA","distance":0,"direction":"N"}
+// ]}
